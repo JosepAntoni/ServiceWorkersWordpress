ac73bb3e5d9cdf2a39358f28386be0b4e5aa01c1
JosepAntoni <costacamps8@gmail.com>
2019-04-30 10:14:18 +0200

    Code refactor on header

--- wp-content/themes/twentynineteen/header.php
+++ wp-content/themes/twentynineteen/header.php
@@ -22,7 +22,6 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-    <a class="is-offline">It's offline</a>
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentynineteen' ); ?></a>
 
 		<header id="masthead" class="<?php echo is_singular() && twentynineteen_can_show_post_thumbnail() ? 'site-header featured-image' : 'site-header'; ?>">
